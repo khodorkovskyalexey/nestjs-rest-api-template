2221e8eb7d8db9a0628f2db71dd63ca7d6ebcd56
Alexey <aezz120813@gmail.com>
2022-04-29 02:18:09 +0600

    refactor: move base-uniq-record-fields.guard to uniq-record-fields.guard file

--- src/common/guards/index.ts
+++ src/common/guards/index.ts
@@ -1,3 +1,2 @@
 export { UserRoleGuard } from './user-role.guard';
-export { BaseUniqRecordFieldsGuard, RecordWithIdInterface } from './base-uniq-record-fields.guard';
-export { UniqRecordFieldsGuard, Constructor } from './uniq-record-fields.guard';
+export { UniqRecordFieldsGuard, Constructor, RecordWithIdInterface } from './uniq-record-fields.guard';
--- src/common/guards/uniq-record-fields.guard.ts
+++ src/common/guards/uniq-record-fields.guard.ts
@@ -1,7 +1,15 @@
-import { CanActivate, ExecutionContext, mixin, Type } from '@nestjs/common';
+import { BadRequestException, CanActivate, ExecutionContext, mixin, Type } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { BaseUniqRecordFieldsGuard, RecordWithIdInterface } from './base-uniq-record-fields.guard';
+import { Request } from 'express';
+import { FindOneOptions, Repository } from 'typeorm';
+
+interface FindOneRecordInterface<T> {
+  findOne: (options?: FindOneOptions<T>) => Promise<T>;
+}
+
+export interface RecordWithIdInterface {
+  id: string;
+}
 
 export type Constructor<T> = new (...args: any[]) => T;
 
@@ -20,4 +28,21 @@ export function UniqRecordFieldsGuard<T extends RecordWithIdInterface>(
   }
 
   return mixin(Guard);
+}
+
+export class BaseUniqRecordFieldsGuard<T extends RecordWithIdInterface> implements CanActivate {
+  constructor(private readonly repository: FindOneRecordInterface<T>, private readonly fields: Array<keyof T>) {}
+
+  async canActivate(context: ExecutionContext, incomingFieldsValue?: Array<Partial<T>>): Promise<boolean> {
+    const req: Request = context.switchToHttp().getRequest();
+    const condition = this.fields.map(
+      (field) => incomingFieldsValue?.find((value) => value[field]) ?? ({ [field]: req.body[field] } as Partial<T>),
+    );
+    const recordWithFieldValue = await this.repository.findOne({ where: condition, select: ['id'] });
+    if (recordWithFieldValue) {
+      throw new BadRequestException(`Record with this conditions already exist`);
+    }
+
+    return true;
+  }
 }
