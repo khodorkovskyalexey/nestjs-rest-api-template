b6c1c7a298572f17da6e8ce0ed9700bc1d26d35f
Alexey <aezz120813@gmail.com>
2022-04-29 01:16:05 +0600

    fix: incoming values type

--- src/common/guards/base-uniq-record-fields.guard.ts
+++ src/common/guards/base-uniq-record-fields.guard.ts
@@ -13,15 +13,10 @@ interface RecordWithIdInterface {
 export class BaseUniqRecordFieldsGuard<T extends RecordWithIdInterface> implements CanActivate {
   constructor(private readonly repository: FindOneRecordInterface<T>, private readonly fields: Array<keyof T>) {}
 
-  async canActivate(
-    context: ExecutionContext,
-    incomingFieldsValue?: Array<Record<keyof T, T[keyof T]>>,
-  ): Promise<boolean> {
+  async canActivate(context: ExecutionContext, incomingFieldsValue?: Array<Partial<T>>): Promise<boolean> {
     const req: Request = context.switchToHttp().getRequest();
     const condition = this.fields.map(
-      (field) =>
-        incomingFieldsValue?.find((value) => value[field]) ??
-        ({ [field]: req.body[field] } as Record<keyof T, T[keyof T]>),
+      (field) => incomingFieldsValue?.find((value) => value[field]) ?? ({ [field]: req.body[field] } as Partial<T>),
     );
     const recordWithFieldValue = await this.repository.findOne({ where: condition, select: ['id'] });
     if (recordWithFieldValue) {
