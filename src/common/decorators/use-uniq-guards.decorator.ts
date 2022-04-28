4ec7261cf5c12091f0890be832e6c40fd608a789
Alexey <aezz120813@gmail.com>
2022-04-29 02:14:02 +0600

    refactor: change function to allow function in decorator

--- src/common/decorators/use-uniq-guards.decorator.ts
+++ src/common/decorators/use-uniq-guards.decorator.ts
@@ -1,6 +1,5 @@
 import { applyDecorators, UseGuards } from '@nestjs/common';
 import { Constructor, RecordWithIdInterface, UniqRecordFieldsGuard } from '../guards';
 
-export function UseUniqGuards<T extends RecordWithIdInterface>(entity: Constructor<T>, fields: Array<keyof T>) {
-  return applyDecorators(UseGuards(UniqRecordFieldsGuard(entity, fields)));
-}
+export const UseUniqGuards = <T extends RecordWithIdInterface>(entity: Constructor<T>, fields: Array<keyof T>) =>
+  applyDecorators(UseGuards(UniqRecordFieldsGuard(entity, fields)));
